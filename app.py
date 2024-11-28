5d5334fb88ee92bf7b1fd7a71b5a65c97bd32b5a
Naveenalla3000 <naveenalla3000@gmail.com>
2024-11-28 19:15:21 +0530

    added files for code deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! a small changes make by naveen alla chowdary'
+    return 'Hello, world! a small changes make by naveen'
 
 if __name__ == '__main__':
     app.run()
