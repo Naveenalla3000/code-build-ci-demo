45c2efd059d4889c74e948b532acf2c386d94259
Naveenalla3000 <naveenalla3000@gmail.com>
2024-11-29 13:25:33 +0530

    small update to show ci-cd

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! a small changes make by naveen alla'
+    return 'Hello, world! a small changes make by naveen a small change to show ci-cd'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000)
